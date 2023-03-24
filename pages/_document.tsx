c81429b0eb8f40f858aea2ce9e406d917a937c4b
akshatgarg12 <akshatarungarg78@gmail.com>
2023-03-24 22:28:17 +0530

    removed materialise

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,29 +3,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-       <Head>
-        <title>Akshat Garg</title>
-        <meta name="description" content="Personal portfolio of akshat garg" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      </Head>
+       <Head title='Akshat Garg'>
+          <title>Akshat Garg</title>
+          <meta name="description" content="Personal portfolio of akshat garg" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        </Head>
       <body>  
-        <nav className="nav-extended">
-          <div className="nav-wrapper">
-            <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-            <ul id="nav-mobile" className="hide-on-med-and-down horizontal-flex-align-center">
-              <li><a href="sass.html">Sass</a></li>
-              <li><a href="badges.html">Components</a></li>
-              <li><a href="collapsible.html">JavaScript</a></li>
-            </ul>
-          </div>
-        </nav>
         <Main />
         <NextScript />
-        <script type="text/javascript" src="js/materialize.min.js"></script>
       </body>
     </Html>
   )
