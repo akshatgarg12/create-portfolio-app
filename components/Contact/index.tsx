e8afc0575ef9f4ce2488c7efd92a649c612628cb
akshatgarg12 <akshatarungarg78@gmail.com>
2023-04-16 20:41:15 +0530

    fix icon sizing issue

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -4,18 +4,19 @@ import { TbBrandGithub, TbBrandLinkedin, TbMail } from "react-icons/tb";
 
 const Contact = ({contact} : {contact : ContactType}) => {
     return (
-        <div className="flex flex-col justify-between rounded-lg text-text p-6 space-y-1.5 md:flex-row">
-            <div className="flex flex-row items-center my-auto space-x-2">
-              <TbMail size={50} />
-              <p>{contact.email}</p>
-            </div>
-            <div className="flex flex-row items-center my-auto space-x-2">
+        <div className="flex flex-col justify-between rounded-lg text-text p-1 space-y-2 md:flex-row">
+           <div className="flex flex-col items-center my-auto">
               <TbBrandGithub size={50} />
               <Link target="_blank" href={contact.github_url}>Github/{contact.github_username}</Link>
             </div>
-            <div className="flex flex-row items-center my-auto space-x-2">
+            <div className="flex flex-col items-center my-auto">
+              <TbMail size={50} />
+              <Link href='#'>{contact.email}</Link>
+            </div>
+  
+            <div className="flex flex-col items-center my-auto">
               <TbBrandLinkedin size={50} />
-              <Link target="_blank" href={contact.linkedin_url}>LinkedIn/{contact.linkedin_username}</Link>
+              <Link target="_blank" href={contact.linkedin_url}>LinkedIn</Link>
             </div>
           </div>
     );
