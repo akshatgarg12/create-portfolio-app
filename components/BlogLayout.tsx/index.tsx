1f98025bc94437974dffecbefb7d64dce61d0529
akshatgarg12 <akshatarungarg78@gmail.com>
2023-05-14 17:36:21 +0530

    added back btn on blog page

--- components/BlogLayout.tsx/index.tsx
+++ components/BlogLayout.tsx/index.tsx
@@ -1,6 +1,9 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { IoArrowBack } from "react-icons/io5";
 
 export default function BlogLayout({ children, meta }: any) {
+  const router = useRouter();
   return (
     <>
       <Head>
@@ -15,6 +18,12 @@ export default function BlogLayout({ children, meta }: any) {
         />
       </Head>
       <div className="bg-background m-0">
+        <IoArrowBack
+          className="text-text cursor-pointer pt-4 pl-4"
+          size={40}
+          title="Go Back"
+          onClick={() => router.back()}
+        />
         <article
           className="prose lg:prose-xl min-h-screen text-text px-8 m-auto py-4 sm:py-16"
           dangerouslySetInnerHTML={{ __html: children }}
