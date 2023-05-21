bb3fe31edbc56fe9a32b9087f85b9b8e9c82314a
akshatgarg12 <akshatarungarg78@gmail.com>
2023-05-21 17:05:41 +0530

    added i18n support to footer and fix navbar i18n

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
 import ThemeBar from "../ThemeBar";
+import { useTranslation } from "next-i18next";
 
 const Footer = () => {
-  // TODO: add i18n
+  const { t } = useTranslation();
   return (
     <div className="min-w-full min-h-[20vh] bg-altBackground flex flex-col justify-center">
       <ThemeBar />
       <h4 className="text-text text-center font-light">
-        Designed and Developed at{" "}
+        {t("footer.text") + " "}
         <Link
           className="text-link"
           href={"https://www.github.com/ToolCity"}
