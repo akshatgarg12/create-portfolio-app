685cbe07fa08072d248c65338968e1368aedf913
akshatgarg12 <akshatarungarg78@gmail.com>
2023-05-21 23:19:45 +0530

    use cdn for pdf worker

--- components/PdfViewer/index.tsx
+++ components/PdfViewer/index.tsx
@@ -1,11 +1,13 @@
+import { useEffect, useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.js",
-  import.meta.url
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 const PdfViewer = ({ file }: { file: string }) => {
+  const [pdfWidth, setPdfWidth] = useState(100);
+  useEffect(() => {
+    setPdfWidth(window.innerWidth * 0.8);
+  }, []);
   return (
     <Document className="flex flex-row justify-center" file={file}>
       <Page
@@ -13,8 +15,8 @@ const PdfViewer = ({ file }: { file: string }) => {
         renderAnnotationLayer={false}
         renderTextLayer={false}
         renderMode="canvas"
-        scale={0.8}
-        width={window.innerWidth}
+        scale={1}
+        width={pdfWidth}
       />
     </Document>
   );
