00d0d6281f44b11a8fcc27c62dec4a10817b6688
akshatgarg12 <akshatarungarg78@gmail.com>
2023-05-28 12:31:07 +0530

    fix blog-cards image

--- components/Cards/Blog/index.tsx
+++ components/Cards/Blog/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export interface BlogCardProps {
   id: string | number;
   title: string;
-  img: string;
+  img?: string;
   description: string;
 }
 
@@ -13,14 +13,17 @@ const BlogCard = (props: BlogCardProps) => {
     <Link href={`/blog/${props.id}`}>
       <div className="flex flex-col p-3 w-full max-w-sm border border-1 border-border rounded-md bg-altBackground text-text cursor-pointer">
         <div>
-          <Image
-            src={props.img}
-            alt={`${props.id}-post-img`}
-            height={150}
-            width={150}
-            className="m-auto"
-            priority
-          />
+          {props.img && (
+            <Image
+              src={props.img}
+              alt={`${props.id}-post-img`}
+              height={150}
+              width={150}
+              style={{ width: "100%", height: "150px", objectFit: "contain" }}
+              className="m-auto"
+              priority
+            />
+          )}
           <h3 className="font-bold text-xl mb-2">{props.title}</h3>
           <p className="font-md text-base">{props.description}</p>
         </div>
