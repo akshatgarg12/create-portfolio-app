2587662bc82118770d9fe866b9d414d0b918b16e
akshatgarg12 <akshatarungarg78@gmail.com>
2023-06-21 15:04:34 +0530

    contact cards revamped

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,33 +1,23 @@
 import { ContactType } from "@/pages";
 import Link from "next/link";
-import { TbBrandGithub, TbBrandLinkedin, TbMail } from "react-icons/tb";
+import { FiGithub, FiMail, FiLinkedin } from "react-icons/fi";
 
 const Contact = ({ contact }: { contact: ContactType }) => {
   return (
-    <div className="flex flex-col justify-center rounded-lg text-text p-1 space-y-10 md:flex-row md:justify-center">
-      <div
-        style={{ margin: "10px" }}
-        className="flex flex-col justify-center items-center min-w-[30%]"
-      >
-        <TbBrandGithub size={30} />
+    <div className="flex flex-row justify-center text-text p-1 space-x-10">
+      <div className="rounded-full bg-background p-4 hover:border hover:border-border">
         <Link target="_blank" href={contact.github_url}>
-          Github/{contact.github_username}
+          <FiGithub size={20} />
         </Link>
       </div>
-      <div
-        style={{ margin: "10px" }}
-        className="flex flex-col justify-center items-center min-w-[30%]"
-      >
-        <TbMail size={30} />
-        <Link href={`mailto:${contact.email}`}>{contact.email}</Link>
+      <div className="rounded-full bg-background p-4 hover:border hover:border-border">
+        <Link target="_blank" href={`mailto:${contact.email}`}>
+          <FiMail size={20} />
+        </Link>
       </div>
-      <div
-        style={{ margin: "10px" }}
-        className="flex flex-col justify-center items-center min-w-[30%]"
-      >
-        <TbBrandLinkedin size={30} />
+      <div className="rounded-full bg-background p-4 hover:border hover:border-border">
         <Link target="_blank" href={contact.linkedin_url}>
-          LinkedIn
+          <FiLinkedin size={20} />
         </Link>
       </div>
     </div>
